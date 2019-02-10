b43812a466b4791432123899f03cb659da66ee20
CNAHYZ <CNAHYZ@users.noreply.github.com>
2019-02-10 23:08:09 +0800

    shiro，权限控制

---
+++ src/main/java/top/flytop/studentsign/controller/admin/LeaveController.java
@@ -0,0 +1,121 @@
+package top.flytop.studentsign.controller.admin;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.flytop.studentsign.dto.BaseResult;
+import top.flytop.studentsign.pojo.Leave;
+import top.flytop.studentsign.service.LeaveService;
+
+import java.util.List;
+
+/**
+ * @ClassName LeaveController
+ * @Description TODO
+ * @Auther Wonder-yz
+ * @Date 2019/1/27 13:50
+ * @Version 1.0
+ */
+@Controller
+@RequestMapping("admin/")
+public class LeaveController {
+    private LeaveService leaveService;
+
+    @Autowired
+    private void leaveService(LeaveService leaveService) {
+        this.leaveService = leaveService;
+    }
+
+    /**
+     * @param param
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO
+     * @date 2019/1/27 19:52
+     */
+    @RequestMapping("getLeave")
+    @ResponseBody
+    public BaseResult getLeave(String param) {
+        return leaveService.getLeave(param);
+    }
+
+    /**
+     * @param param
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO
+     * @date 2019/1/27 19:52
+     */
+    @RequestMapping("leaveStatusFilter")
+    @ResponseBody
+    public BaseResult leaveStatusFilter(String param) {
+        return leaveService.getLeaveByStatus(param);
+    }
+
+    /**
+     * @param id
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO
+     * @date 2019/1/27 20:30
+     */
+    @RequestMapping("removeLeave")
+    @ResponseBody
+    public BaseResult removeLeave(Integer id) {
+        return leaveService.removeLeave(id);
+    }
+
+    /**
+     * @param id
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO
+     * @date 2019/1/28 15:23
+     */
+    @RequestMapping("getLeaveDetail")
+    @ResponseBody
+    public BaseResult getLeaveDetail(Integer id) {
+        return leaveService.getLeaveDetailById(id);
+    }
+
+    /**
+     * @param id
+     * @param status
+     * @param refusalReason
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO
+     * @date 2019/1/28 21:58
+     */
+    @RequestMapping("examineLeave")
+    @ResponseBody
+    public BaseResult examineLeave(Integer id, Integer status, String refusalReason) {
+        Leave leave = new Leave();
+        leave.setId(id);
+        leave.setAuditStatus(status);
+        if (status == 2)
+            leave.setRefusalReason(refusalReason);
+        System.out.println(leave);
+        return leaveService.examineLeave(leave);
+    }
+
+    /**
+     * @param
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO
+     * @date 2019/1/28 21:58
+     */
+    @RequestMapping("getNoExamineLeave")
+    @ResponseBody
+    public BaseResult getNoExamineLeave(@RequestParam(defaultValue = "1") Integer pageNo) {
+        int pageSize = 1;
+        PageHelper.startPage(pageNo, pageSize);
+        BaseResult filterResult = leaveService.getLeaveByStatus("0");
+        if (filterResult.isSuccess()) {
+            List<Leave> leaveList = (List) filterResult.getData();
+            PageInfo<Leave> page = new PageInfo<>(leaveList);
+            return new BaseResult<>(true, page);
+        } else
+            return new BaseResult(false, 1, "暂无信息");
+    }
+
+}
---
+++ src/main/java/top/flytop/studentsign/controller/admin/SignInController.java
@@ -0,0 +1,62 @@
+package top.flytop.studentsign.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.flytop.studentsign.dto.BaseResult;
+import top.flytop.studentsign.service.SignInService;
+
+/**
+ * @ClassName signInController
+ * @Description TODO
+ * @Auther Wonder-yz
+ * @Date 2019/1/12 17:18
+ * @Version 1.0
+ */
+@Controller
+@RequestMapping("admin/")
+public class SignInController {
+    @Autowired
+    private SignInService signInService;
+
+    /**
+     * @param
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO
+     * @date 2019/1/12 17:19
+     */
+    @RequestMapping(value = "signList", method = RequestMethod.POST)
+    @ResponseBody
+    public BaseResult signList(String dayNum) {
+
+        return signInService.getSignRecord(dayNum);
+    }
+
+    /**
+     * @param startTime
+     * @param endTime
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO 自定义日期过滤操作
+     * @date 2019/1/26 16:59
+     */
+    @RequestMapping(value = "signListFilter", method = RequestMethod.POST)
+    @ResponseBody
+    public BaseResult signListFilter(String startTime, String endTime) {
+        return signInService.getSignRecordFilter(startTime, endTime);
+    }
+
+    /**
+     * @param keyword
+     * @return top.flytop.studentsign.dto.BaseResult
+     * @Description TODO 签到列表搜索操作
+     * @date 2019/1/26 17:01
+     */
+    @RequestMapping("searchSignList.do")
+    @ResponseBody
+    public BaseResult searchSignList(String keyword) {
+        return signInService.getSignRecordByKeyword(keyword);
+    }
+
+}
